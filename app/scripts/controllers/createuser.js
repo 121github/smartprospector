06a1918e4052f850211bd730a395003abcf78155
Stuart Williams <stuartw@121customerinsight.co.uk>
2014-02-19 13:50:21 +0000

    Some styling changes to the users page. Changed the role filter to use radio buttons instead of checkboxes. Added some form validation to the create users page

--- app/scripts/controllers/createuser.js
+++ app/scripts/controllers/createuser.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('smartprospectorApp')
-  .controller('CreateUserCtrl', function ($scope, Role) {
+  .controller('CreateUserCtrl', function ($rootScope, $scope, Role) {
     
+    $rootScope.pageTitle = 'Create User';
     $scope.loading = true;
+
     $scope.roles = Role.query(function () {
+      if ($scope.roles.length > 0) {
+        $scope.user = {role: $scope.roles[0].id};
+      }
       $scope.loading = false;
     });
+    
+    $scope.doCreateUser = function (user) {
+      alert(user);
+    };
     
   });
--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -4,36 +4,21 @@ angular.module('smartprospectorApp')
   .controller('UsersCtrl', function ($rootScope, $scope, User) {
     
     $rootScope.pageTitle = 'Users';
-    $scope.roles         = [];
-    var distinctRoles    = [];
-    var selectedRoles    = [];
+    $scope.roles         = ['All'];
+    $scope.filterRole    = {selected: 'All'};
     $scope.loading       = true;
     
     $scope.users = User.query({action: 'all'}, function () {
       $scope.users.forEach(function(user) {
-        if (distinctRoles.indexOf(user.role) === -1) {
-          distinctRoles.push(user.role);
-          $scope.roles.push({role: user.role, checked: false});
+        if ($scope.roles.indexOf(user.role) === -1) {
+          $scope.roles.push(user.role);
         }
       });
-      selectedRoles = angular.copy(distinctRoles);
       $scope.loading = false;
     });
     
-    $scope.toggleRoleFilter = function () {
-      selectedRoles = angular.copy(distinctRoles);
-      $scope.roles.forEach(function (role) {
-        if (!role.checked) {
-          selectedRoles.splice(selectedRoles.indexOf(role.role), 1);
-        }
-      });
-      if (selectedRoles.length === 0) {
-        selectedRoles = angular.copy(distinctRoles);
-      }
-    };
-    
     $scope.filterByRole = function(user) {
-      return (selectedRoles.indexOf(user.role) !== -1);
+      return $scope.filterRole.selected === 'All' ? true : user.role === $scope.filterRole.selected ? true : false;
     };
     
   });
