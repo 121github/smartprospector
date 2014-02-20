f5784aba4afb30c9f99d91b984633e1caf7a2619
Brad Foster <bradf@121customerinsight.co.uk>
2014-02-20 14:22:49 +0000

    anything

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,6 +1,28 @@
 'use strict';
 
-angular.module('smartprospectorApp')
-  .controller('SearchCtrl', function ($rootScope, $scope) {
+angular.module('smartprospectorApp').controller('SearchCtrl', function ($rootScope, $scope) {
     $rootScope.pageTitle = 'Search';
+    
+    $rootScope.items = [
+        {name:'Companies',desc:'Search for a business'}, 
+        {name:'Contact',desc:'Search for a person'},
+        {name:'Map',desc:'Search for an address'}
+      ];
+      
+      //set the default search type
+      $rootScope.name = "Companies";
+      $rootScope.description = "Search for a business";
+      
+      $rootScope.selectItem = function(item){
+        $rootScope.name = item.name;
+        $rootScope.description = item.desc;
+      };
+    
+    
+    //sector multiselect
+    $scope.sectors = [{id:1, name: 'Food and Drink'}, {id:2, name: 'Industrial'}, {id:1, name: 'Technology'}];
+    $scope.selectedSectors = [];
+        //sector multiselect
+    $scope.status = [{id:1, name: 'New'}, {id:2, name: 'In Progress'}, {id:1, name: 'Complete'}];
+    $scope.selectedStatus = [];
   });
