a5d5fed668a1b9e39f19a2b2a4e1d4927013a61d
Brad Foster <bradf@121customerinsight.co.uk>
2014-02-27 14:48:41 +0000

    fixed formatting on search pages

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -3,10 +3,10 @@
 
 angular.module('smartprospectorApp').controller('SearchCtrl', function($scope) {
   $scope.pageTitle = 'Search';
-    $scope.loading = false;
+  $scope.loading = false;
 });
 
- function searchCtrl($scope){
+function searchCtrl($scope) {
   $scope.items = [
     {type: 'Companies', desc: 'Search for a business'},
     {type: 'Contacts', desc: 'Search for a person'},
@@ -23,34 +23,34 @@ angular.module('smartprospectorApp').controller('SearchCtrl', function($scope) {
     $scope.search.selectedOne = [];
     $scope.search.selectedTwo = [];
     $scope.search.selectedThree = [];
-    
-    
+
+
     console.log($scope.search);
     $scope.type = item.type;
     $scope.description = item.desc;
-         //clear the selections
-      $scope.search = $scope.reset;
-      
-       if (item.type === "Companies") {
+    //clear the selections
+    $scope.search = $scope.reset;
+
+    if (item.type === "Companies") {
       $scope.selectOneTitle = "Sector";
       $scope.selectTwoTitle = "Status";
       $scope.selectThreeTitle = "Outcome";
 
-      
-  //sector multiselect
-  $scope.selectOne = [{id: 1, name: 'Food and Drink'}, {id: 2, name: 'Industrial'}, {id: 3, name: 'Technology'}];
-  //sector multiselect
-  $scope.selectTwo = [{id: 1, name: 'New'}, {id: 2, name: 'In Progress'}, {id: 3, name: 'Complete'}];
-  //sector outcomes
-  $scope.selectThree = [{id: 1, name: 'No Answer'}, {id: 2, name: 'Not Interested'}, {id: 3, name: 'Call Back'}];
-      
+
+      //sector multiselect
+      $scope.selectOne = [{id: 1, name: 'Food and Drink'}, {id: 2, name: 'Industrial'}, {id: 3, name: 'Technology'}];
+      //sector multiselect
+      $scope.selectTwo = [{id: 1, name: 'New'}, {id: 2, name: 'In Progress'}, {id: 3, name: 'Complete'}];
+      //sector outcomes
+      $scope.selectThree = [{id: 1, name: 'No Answer'}, {id: 2, name: 'Not Interested'}, {id: 3, name: 'Call Back'}];
+
     }
     else if (item.type === "Contacts") {
 
       $scope.selectOneTitle = "Position";
       $scope.selectTwoTitle = "Department";
       $scope.selectThreeTitle = "Income";
-      
+
       //first multiselect
       $scope.selectOne = [{id: 1, name: 'Director'}, {id: 2, name: 'Manager'}, {id: 3, name: 'Consultant'}];
       //second multiselect
@@ -65,8 +65,8 @@ angular.module('smartprospectorApp').controller('SearchCtrl', function($scope) {
   $scope.selectTwoTitle = "Status";
   $scope.selectThreeTitle = "Outcome";
   //clear the selections
-  $scope.search = [{selectedOne:[],selectedTwo:[],selectedThree:[]}];
-  
+  $scope.search = [{selectedOne: [], selectedTwo: [], selectedThree: []}];
+
   //sector multiselect
   $scope.selectOne = [{id: 1, name: 'Food and Drink'}, {id: 2, name: 'Industrial'}, {id: 3, name: 'Technology'}];
   //sector multiselect
@@ -78,22 +78,24 @@ angular.module('smartprospectorApp').controller('SearchCtrl', function($scope) {
 
   $scope.displayIcon = 'glyphicon-minus';
   $scope.displayPanel = true;
-  
-  $scope.togglePanel = function(panelName){
-    if(panelName===false){
-    $scope.displayIcon = "glyphicon-minus"; } else {
-    $scope.displayIcon = "glyphicon-plus";
+
+  $scope.togglePanel = function(panelName) {
+    if (panelName === false) {
+      $scope.displayIcon = "glyphicon-minus";
+    } else {
+      $scope.displayIcon = "glyphicon-plus";
     }
   };
 }
-  function resultsCtrl($scope){
+function resultsCtrl($scope) {
   $scope.displayIcon = 'glyphicon-plus';
   $scope.displayPanel = true;
-    
-   $scope.togglePanel = function(panelName){
-    if(panelName===true){
-    $scope.displayIcon = "glyphicon-minus"; } else {
-    $scope.displayIcon = "glyphicon-plus";
+
+  $scope.togglePanel = function(panelName) {
+    if (panelName === true) {
+      $scope.displayIcon = "glyphicon-minus";
+    } else {
+      $scope.displayIcon = "glyphicon-plus";
     }
   };
-  }
+}
